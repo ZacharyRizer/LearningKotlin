13889655d6cd900c354b2371bde8299354e1b172
ZacharyRizer <zacharyrizer@gmail.com>
2024-02-07 15:31:07 -0500

    Variable types

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,13 @@
-fun callName(name: String) {
-    println("The name is $name.")
+fun types(): String {
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long = 100_000_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
+    return "Different Types: $a, $b, $c, $d, $e, $f"
 }
 
 fun main() {
-    val newName = "Tom"
-    callName(newName)
+    println(types())
 }

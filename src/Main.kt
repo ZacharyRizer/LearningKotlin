e10be03f95212d81ca8eb600f31ba53c82e1e9d9
ZacharyRizer <zacharyrizer@gmail.com>
2024-02-07 16:10:58 -0500

    Collections

--- src/Main.kt
+++ src/Main.kt
@@ -8,6 +8,21 @@ fun types(): String {
     return "Different Types: $a, $b, $c, $d, $e, $f"
 }
 
+fun collections() {
+    val greenNumbers = listOf(1, 4, 23)
+    val redNumbers = listOf(17, 2)
+    println("${greenNumbers.size + redNumbers.size}")
+
+    val SUPPORTED = setOf("HTTP", "HTTPS", "FTP")
+    val requested = "smtp"
+    val isSupported = requested.uppercase() in SUPPORTED
+    println("Support for $requested: $isSupported")
+
+    val number2word = mapOf(1 to "One", 2 to "Two", 3 to "Three")
+    val n = 2
+    println("$n is spelt as ${number2word[n]}")
+}
+
 fun main() {
-    println(types())
+    collections()
 }
